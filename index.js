f61703ffeb3650433ae534126f311966d039f0d4
Anwesh43 <anweshthecool0@gmail.com>
2020-03-13 03:14:35 +0530

    adding State classs

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const w = window.innerWidth, h = window.innerHeight, scGap = 0.02, delay = 40
+
+class State {
+
+    scale = 0
+
+    update(cb) {
+        this.scale += scGap
+        if (this.scale > 1) {
+            this.scale = 0
+            cb()
+        }
+    }
+}
 const vueInstance = new Vue({
     el : '#app'
 })
